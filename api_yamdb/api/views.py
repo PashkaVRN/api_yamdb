bbc92a95f70508bc3e518ac38db37c33473df07b
Дмитрий Кузнецов <dimanner.k@yandex.ru>
2022-08-09 22:19:30 +0300

    Удаление веточкиÐ

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,7 +10,7 @@ from .serializers import (CategorySerializer, GenreSerializer,
 from .serializers import (CommentSerializer, ReviewSerializer, SignUpSerializer)
 
 from reviews.models import Category, Genre, Review, Title
-from reviews.models import Comment, Review
+
 
 
 class CategoryViewSet(MixinSet):
@@ -42,36 +42,4 @@ class TitleViewSet(viewsets.ModelViewSet):
         return TitleGetSerializer
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
-    #permission_classes = () разрешения
 
-    def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        return title.reviews.all()
-
-    def perform_create(self, serializer):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        serializer.save(author=self.request.user, title=title)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
-    # permission_classes = () разрешения
-
-    def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        return review.comments.all()
-
-    def perform_create(self, serializer):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        serializer.save(author=self.request.user, review=review)
-
-
-class SignUpView(APIView):
-    """
-    Запрос создания нового пользователя.
-    Отправка на email кода подтверждения региcтрации.
-    """
-    def post(self, request):
-        serializer = SignUpSerializer(data=request.data)

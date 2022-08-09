157971130b83d02833b989ad304ce31b7f3da5c1
Павел Шевель <pawel.pavlenti@yandex.ru>
2022-08-09 19:38:08 +0300

    Добавил модели, вьюсеты, сирилизатор для comment и review

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from reviews.models import Review, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    scope = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    titles = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        unique = ('author', 'title')
+
+    def __str__(self):
+        return self.text
 
 
 class Comment(models.Model):
@@ -9,7 +35,15 @@ class Comment(models.Model):
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
-        related_name='comments')
+        related_name='comments'
+    )
     text = models.TextField()
     created = models.DateTimeField(
         'Дата комментария', auto_now_add=True,)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
+

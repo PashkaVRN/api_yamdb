e5821c596fe90eda44e9e8eb70091ffd3cc35b01
Андрей Малёванный <redsonja89@gmail.com>
2022-08-09 14:47:23 +0500

    Yandex Practikum HW11 Added custon User Model:     altered email field:     added bio and role fields.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,36 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db.models import CharField, EmailField, TextField
 
 
 class User(AbstractUser):
-    pass
+    """
+    Модель пользователя.
+    Дополнительные поля биографии и ролей.
+    Возможные роли: юзер, модератор, админ.
+    Новым пользователям по умолчанию присваивается роль юзер.
+    Определяет права редактирования контента для разных ролей.
+    """
+    USER_ROLE = 'user'
+    MODERATOR_ROLE = 'moderator'
+    ADMIN_ROLE = 'admin'
+    ROLE_CHOICES = (
+        (USER_ROLE, 'user'),
+        (MODERATOR_ROLE, 'moderator'),
+        (ADMIN_ROLE, 'admin')
+    )
+    email = EmailField(
+        unique=True,
+        verbose_name='Адрес электронной почты',
+        help_text='Введите адрес электронной почты')
+    bio = TextField(
+        blank=True,
+        verbose_name='Биография пользователя',
+        help_text='Кратко опишите свою биографию'
+    )
+    role = CharField(
+        max_length=9,
+        choices=ROLE_CHOICES,
+        default=USER_ROLE,
+        verbose_name='Пользовательская роль',
+        help_text='Выберите роль пользователя'
+    )

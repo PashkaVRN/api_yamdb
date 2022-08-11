bc90765a9bd44670b4fd1056267021779fe1b31e
Павел Шевель <pawel.pavlenti@yandex.ru>
2022-08-11 22:37:34 +0300

    test

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -44,30 +44,23 @@ class CommentViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(MixinSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    #permission_classes = (IsAdminOrReadOnly, )
-    pagination_class = PageNumberPagination
+    pagination_class = LimitOffsetPagination
     search_fields = ('=name', )
     lookup_field = 'slug'
 
 
 class GenreViewSet(MixinSet):
     queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-    #permission_classes = (IsAdminOrReadOnly, )
-    pagination_class = PageNumberPagination
+    pagination_class = LimitOffsetPagination
     search_fields = ('=name', )
     lookup_field = 'slug'
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
-    #permission_classes = (IsAdminOrReadOnly, )
-    pagination_class = PageNumberPagination
+    pagination_class = LimitOffsetPagination
 
     def get_serializer_class(self):
         if self.action in ('create', 'partial_update'):
             return TitlePostSerializer
         return TitleGetSerializer
-
-
-

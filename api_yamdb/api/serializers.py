11197a33e87e70d8eaa3adbee2a4580c2e3b7eb2
Дмитрий Кузнецов <dimanner.k@yandex.ru>
2022-08-11 23:30:35 +0300

    проверка

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,112 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from reviews.models import Category, Comment, Genre, Review, Title
+
+from rest_framework.serializers import (CharField, EmailField, Serializer,
+                                        ValidationError)
+
+User = get_user_model()
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Класс сериализатор категории."""
+    class Meta:
+        fields = ('name', 'slug',)
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Класс сериализатор жанра."""
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Класс сериализатор получения произведений."""
+    genre = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="slug",
+        many=True
+    )
+    category = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="slug",
+        many=False
+    )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+
+    )
+
+    class Meta:
+        fields = '__all__'
+
+        model = Title
+
+
+class TitleCreateSerializer(serializers.ModelSerializer):
+    """Класс сериализатор создания произведений."""
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        many=True,
+        queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+
+        model=Comment
+    )
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+
+    )
+
+    class Meta:
+        fields = '__all__'
+
+        model = Title
+
+        model = Review
+
+
+class SignUpSerializer(Serializer):
+    """Сериализатор запроса регистрации нового пользователя."""
+    email = EmailField(max_length=254)
+    username = CharField(max_length=150)
+
+    def validate_username(self, value):
+        """Проверяет, что 'me' не используется как username."""
+        if value == 'me':
+            raise ValidationError(
+                'Нельзя использовать "me" в качестве имени пользователя'
+            )
+        return value
+
+    def validate_email(self, value):
+        """
+        Проверяет, что email не используется
+        другими зарегестрированными пользователями.
+        """
+        if User.objects.filter(email=value).exists():
+            raise ValidationError(
+                f'Email {value} уже использется'
+            )
+        return value
+
+
+class GetJWTTokenSerializer(Serializer):
+    """Сериализатор запроса JWT токена."""
+    username = CharField(max_length=150)
+    confirmation_code = CharField()

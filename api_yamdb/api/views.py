e940d61c97d22d444b394ac0ff73cfe6bc5e9b28
Дмитрий Кузнецов <dimanner.k@yandex.ru>
2022-08-11 22:17:41 +0300

    Настрока

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,5 @@
-from rest_framework import filters, viewsets, request
+from rest_framework import viewsets
 
-from rest_framework.filters import SearchFilter
-from rest_framework.views import APIView
-from rest_framework.pagination import LimitOffsetPagination
-
-from .filters import TitleFilter
 from .mixins import MixinSet
 from .serializers import (CategorySerializer, GenreSerializer,
                           TitleSerializer, TitleCreateSerializer)
@@ -12,47 +7,42 @@ from .serializers import (CategorySerializer, GenreSerializer,
 from reviews.models import Category, Genre, Review, Title
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
-    #permission_classes = () разрешения
-    pagination_class = LimitOffsetPagination
-
-
 class CategoryViewSet(MixinSet):
-    """Класс категория, доступно только админу."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    #permission_classes = (IsAdminOrReadOnly, )
     pagination_class = LimitOffsetPagination
-    filter_backends = (filters.SearchFilter,)
     search_fields = ('=name', )
     lookup_field = 'slug'
 
     def get_permissions(self):
-        if request.user.is_superuser or request.user.role == 'admin':
-            return self.request.method == 'POST' or 'DELETE'
+        if self.request.method == 'POST':
+            self.permission_classes = (Admin, )
+        elif self.request.method == 'DELETE':
+            self.permission_classes = (Admin, )
         return super().get_permissions()
 
 
 class GenreViewSet(MixinSet):
-    """Класс жанр, доступно только админу."""
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    #permission_classes = (IsAdminOrReadOnly, )
     pagination_class = LimitOffsetPagination
-    filter_backends = (filters.SearchFilter,)
     search_fields = ('=name', )
     lookup_field = 'slug'
 
     def get_permissions(self):
-        if request.user.is_superuser or request.user.role == 'admin':
-            return self.request.method == 'POST' or 'DELETE'
+        if self.request.method == 'POST':
+            self.permission_classes = (Admin, )
+        elif self.request.method == 'DELETE':
+            self.permission_classes = (Admin, )
         return super().get_permissions()
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    """Класс произведения, доступно только админу."""
     queryset = Title.objects.all()
+    #permission_classes = (IsAdminOrReadOnly, )
     pagination_class = LimitOffsetPagination
-    filterset_class = TitleFilter
 
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PATCH',):
@@ -60,6 +50,8 @@ class TitleViewSet(viewsets.ModelViewSet):
         return TitleSerializer
 
     def get_permissions(self):
-        if request.user.is_superuser or request.user.role == 'admin':
-            return self.request.method == 'POST' or 'DELETE'
+        if self.request.method == 'POST':
+            self.permission_classes = (Admin, )
+        elif self.request.method == 'DELETE':
+            self.permission_classes = (Admin, )
         return super().get_permissions()

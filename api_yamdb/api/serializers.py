cbf26a0a3b2f838e62afd6787dff777485143fa8
Павел Шевель <pawel.pavlenti@yandex.ru>
2022-08-12 13:44:30 +0300

    поправил сирилизатор комментов

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from reviews.models import Category, Comment, Genre, Review, Title
 
@@ -37,6 +39,19 @@ class CommentSerializer(serializers.ModelSerializer):
         read_only=True,
         slug_field='username'
     )
+    review = serializers.SlugRelatedField(slug_field='text', read_only=True)
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (
+            request.method == 'POST'
+            and Review.objects.filter(title=title, author=author).exists()
+        ):
+            raise ValidationError('Можно оставить только один отзыв')
+        return data
 
     class Meta:
         fields = '__all__'

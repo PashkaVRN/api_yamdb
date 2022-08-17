76fafaa9b7d929567d89326efaf34814dca3b3d8
Андрей Малёванный <redsonja89@gmail.com>
2022-08-17 17:53:21 +0500

    2nd review corrections

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -8,11 +8,13 @@ def username_validation(value):
     Нельзя использовать имя пользователя me.
     Допускается использовать только буквы, цифры и символы @ . + - _.
     """
-    checked_value = re.match(r'^[\w.@+-]+', value)
     if value == 'me':
         raise ValidationError('Нельзя использовать "me" как имя пользователя')
-    elif checked_value.group() != value:
-        forbidden_simbol = value[checked_value.span()[1]]
+    checked_value = re.match('^[\w.@+-]+', value)
+    if checked_value is None or checked_value.group() != value:
+        forbidden_simbol = value[0] if (
+            checked_value is None
+        ) else value[checked_value.span()[1]]
         raise ValidationError(f'Нельзя использовать символ {forbidden_simbol} '
                               'в username. Имя пользователя может содержать '
                               'только буквы, цифры и символы @ . + - _.')
